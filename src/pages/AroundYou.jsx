13cf53dd9474c62ad7485473cf3905454fc46f14
Ishan <ishanwijes@gmail.com>
2022-11-14 06:35:38 +1100

    added geolocation api call

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components'
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
-export default CountryTracks;
+
+
+const AroundYou = () => {
+  const [country, setCountry] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  const { activeSong, isPlaying } = useSelector(state => state.player)
+
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country)
+  
+
+
+  useEffect(() => {
+
+    fetch('https://ipinfo.io/json?token=93cd9a555aabe8')
+    .then(res => res.json())
+    .then(data => {
+      setCountry(data.country)
+    })
+    .catch(err => console.log(err))
+    .finally(() => setLoading(false))
+
+  }, [country])
+  
+  
+
+  if(isFetching && loading) return <Loader title="Loading songs in your country..."/>
+
+  if(error && country) return <Error />
+
+
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around You</h2>
+    </div>
+  )
+}
+
+export default AroundYou;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -22,7 +22,8 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({ query: () => '/charts/world'}),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` })
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` })
   })
 })
 
@@ -30,5 +31,6 @@ export const {
   useGetTopChartsQuery,
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
-  useGetArtistDetailsQuery
+  useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery
 } = shazamCoreApi
