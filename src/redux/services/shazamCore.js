17d78d574b174e886057f2ce629a99f3d8d25a1c
Ishan <ishanwijes@gmail.com>
2022-11-15 02:06:42 +1100

    search functionality completed

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -24,7 +24,8 @@ export const shazamCoreApi = createApi({
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` })
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` })
   })
 })
 
@@ -34,5 +35,6 @@ export const {
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
   useGetSongsByCountryQuery,
-  useGetSongsByGenreQuery
+  useGetSongsByGenreQuery,
+  useGetSongsBySearchQuery
 } = shazamCoreApi
