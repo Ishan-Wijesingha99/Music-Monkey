9b3f9e4684c694c864b73f1b9564cf47f3003314
Ishan <ishanwijes@gmail.com>
2022-11-24 02:01:56 +1100

    changed ArtistCard component to be congruent with SongCard component

--- src/components/smallerComponents/ArtistCard.js
+++ src/components/smallerComponents/ArtistCard.js
@@ -8,17 +8,17 @@ export const ArtistCard = ({ track }) => {
 
   return (
     <div
-    className="flex flex-col w-[180px] rounded-lg cursor-pointer m-4 bg-[#a56d6d] p-1 hover:scale-105 duration-200"
+    className="flex flex-col w-[184px] rounded-lg cursor-pointer m-4 hover:scale-105 duration-200 mb-8"
     onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
     >
 
       <img
       src={track?.images?.coverart}
       alt="artist"
-      className="h-[172px] w-[172px] duration-200 rounded-lg"
+      className="h-[176px] w-[176px] duration-200 rounded-lg p-1 bg-[#a56d6d]"
       />
 
-      <p className="mt-4 mb-1 font-semibold text-lg text-white truncate">
+      <p className="mt-4 mb-1 font-semibold text-lg text-white truncate hover:underline">
         { track?.subtitle.length > 24 ? track?.subtitle.slice(0, 24).concat('...') : track?.subtitle }
       </p>
 
